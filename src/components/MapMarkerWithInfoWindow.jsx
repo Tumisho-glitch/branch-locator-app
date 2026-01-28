31e4c803a3de0ca251874b8956aaea2ab8c56956
Tumisho <fortuno.9.tf@gmail.com>
2026-01-28 10:06:43 +0200

    removed .env file from being tracked

--- src/components/MapMarkerWithInfoWindow.jsx
+++ src/components/MapMarkerWithInfoWindow.jsx
@@ -24,7 +24,6 @@ export default function MapMarkerWithInfoWindow({lat, lng}) {
                         <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIgyU3fDk3HZadI9L0eYRwkZ7OpXJdv7B55w&s"} alt={"image"} width={200} />
                         <div>
                             <p>This an official capitec branch</p>
-                            <h2>Please visit to conduct in person business</h2>
                             <h2>Trading hours</h2>
                             <h3>09:00-17:00 Monday-Friday</h3>
                             <h3>09:00-15:00 Saturday</h3>
