bb111ccfa9b98eaacecdcc64debbf0565821112a
Tumisho <fortuno.9.tf@gmail.com>
2026-01-29 12:28:37 +0200

    Implemented search place and center functionality

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,62 +1,100 @@
 "use client";
-import {AdvancedMarker, APIProvider, InfoWindow, Map, Pin, useAdvancedMarkerRef} from "@vis.gl/react-google-maps";
+import {
+    AdvancedMarker,
+    APIProvider,
+    InfoWindow,
+    Map,
+    Pin,
+    useAdvancedMarkerRef,
+    useMap
+} from "@vis.gl/react-google-maps";
 import {useEffect, useState} from "react";
 import MapMarkerWithInfoWindow from "./MapMarkerWithInfoWindow.jsx";
+import branchesData from "../../public/data/branches.json";
 
 
-export default function MapComponent(){
+export default function MapComponent({focusLocation, setFocusLocation}) {
 
-    const [userPosition, setUserPosition] = useState(null);
+    const [focusPosition, setFocusPosition] = useState(null);
+    const [branches, setBranches] = useState([]);
     const [error, setError] = useState(null);
     const [markerRef, marker] = useAdvancedMarkerRef();
+    const map = useMap();
+
+                useEffect(() => {
+
+                    console.log("The focused location has changed")
+
+                    if(!navigator.geolocation){
+                        setError("Geolocation is not supported, try different browser");
+                        return;
+                    }
+
+                    //get the user's gps coordinates
+                    if(!focusLocation){
+                        navigator.geolocation.getCurrentPosition(position => {
+                                console.log(position.coords.latitude, position.coords.longitude);
+                                setFocusPosition({lat: position.coords.latitude, lng: position.coords.longitude});
+                            },
+                            error => {
+                                setError(error)
+                            },{
+                                enableHighAccuracy: true,
+                                maximumAge: 0
+                            })
+                    }
+                    else
+                    {
+                        //if the focus location is given, the set the focusPosition to be the focusLocation
+                        setFocusPosition(focusLocation)
+                    }
+
+                    if(!map || !focusLocation) return;
+                    console.log("Focus Location", focusLocation);
+                    map.panTo(focusLocation)
+
+
+                },[map, setError, focusLocation])
 
     useEffect(() => {
 
-        if(!navigator.geolocation){
-            setError("Geolocation is not supported, try different browser");
-            return;
+        async function loadBranches(){
+            const data = await fetch("/data/branches.json");
+            const branches = await data.json();
+
+            setBranches(branches);
+
         }
 
-        //get the user's gps coordinates
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position.coords.latitude, position.coords.longitude);
-            setUserPosition({lat: position.coords.latitude, lng: position.coords.longitude});
-        },
-            error => {
-                setError(error)
-            },{
-            enableHighAccuracy: true,
-                maximumAge: 0
-            })
+        loadBranches();
+    }, [])
 
-    },[setError])
 
-    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
 
-    if(!userPosition){
-        return <p>Waiting for user position...</p>
+    if(!focusPosition || !branches){
+        return <p>Waiting for user position or branches data...</p>
     }
 
-    console.log(userPosition)
+
 
     return (
-        <APIProvider apiKey={apiKey}>
+
             <Map
                 style={{width: '90vw', height: '100vh'}}
-                defaultCenter={userPosition}
-                defaultZoom={10}
+                defaultCenter={focusPosition}
+                defaultZoom={13}
                 gestureHandling='greedy'
                 disableDefaultUI
                 mapId={"68d9a5bef583da965240143d"}
             >
-                
-                <MapMarkerWithInfoWindow lat={-26.2041} lng={28.0473} address={"Location: 6 university road, Campus square"}
-                                         tradingHours={["09:00-17:00 Monday-Friday", "09:00-15:00 Saturday"]} contact={"072 CAPITEC BANK"}/>
-                <MapMarkerWithInfoWindow lat={-33.9321} lng={18.8602} address={"Location: 6 university road, Campus square"}
-                                         tradingHours={["09:00-17:00 Monday-Friday", "09:00-15:00 Saturday"]} contact={"072 CAPITEC BANK"}/>
 
+                {
+                    branches.map(branch => {
+                        return <MapMarkerWithInfoWindow lat={branch.lat} lng={branch.lng} address={branch.address}
+                                                 tradingHours={branch.tradingHours} contact={branch.contact}/>
+                    })
+                }
 
             </Map>
-    </APIProvider>
     )
 }
--- src/pages/BranchLocatorPage.jsx
+++ src/pages/BranchLocatorPage.jsx
@@ -1,10 +1,87 @@
 import MapComponent from "../components/MapComponent.jsx";
+import {useState} from "react";
+import {APIProvider} from "@vis.gl/react-google-maps";
 
 export default function BranchLocatorPage() {
+    const [focusLocation, setFocusLocation] = useState("");
+
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
+    return (
+        <div className={"flex flex-col justify-center items-center w-screen space-y-2"}>
+            <h1 className="w-full  text-center">Branch locator page</h1>
+            <SearchBar setFocusLocation={setFocusLocation} />
+            <APIProvider apiKey={apiKey}>
+                <MapComponent focusLocation={focusLocation} setFocusLocation={setFocusLocation} />
+            </APIProvider>
+        </div>
+    )
+}
+
+function SearchBar({setFocusLocation}) {
+    const [query, setQuery] = useState("");
+    const [locations, setLocations] = useState([]);
+
+    function handleQueryChange(event) {
+        const newQuery = event.target.value;
+        setQuery(newQuery);
+
+        // then submit a request to find all places matching the query
+        searchPlaces(newQuery);
+
+
+    }
+
+    async function searchPlaces(SearchQuery, setFocusLocation){
+        const res = await fetch(`https://places.googleapis.com/v1/places:searchText`, {
+            method: "POST",
+            body: JSON.stringify({"textQuery": SearchQuery}),
+            headers: {
+                "Content-Type": "application/json",
+                "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
+                "X-Goog-FieldMask": "places.displayName,places.location"
+            }
+        })
+        const data = await res.json();
+        setLocations(data.places || []);
+        console.log("Data :", data.places);
+    }
+
+    function clearSearch(){
+        setQuery("")
+        setLocations("")
+    }
+
     return (
         <div>
-            Branch locator page
-            <MapComponent />
+            <input className="bg-stone-100 w-200" onChange={e => handleQueryChange(e)} value={query} />
+            {
+                locations && locations.length > 0 &&
+                <SearchResults locations={locations} setFocusLocation={setFocusLocation} clearSearch={clearSearch}/>
+            }
+
+        </div>
+
+    )
+}
+
+function SearchResults({locations, setFocusLocation, clearSearch}) {
+    console.log("Locations :", locations);
+
+    function handleSetFocusLocation(location){
+        //get the location's latitude and longitude
+        console.log("This is the chosen location:", location.location);
+        setFocusLocation({lat: location.location.latitude, lng: location.location.longitude});
+        clearSearch();
+    }
+
+    return (
+        <div className="bg-stone-300 space-y-2 font-semibold fixed z-10 w-200 text-center shadow-2xl">
+            {
+               locations.map((location) => {
+                    return (<p className="hover:bg-stone-400 hover:cursor-pointer" onClick={()=>handleSetFocusLocation(location)}>{location.displayName.text}</p>)
+                })
+            }
         </div>
     )
 }
